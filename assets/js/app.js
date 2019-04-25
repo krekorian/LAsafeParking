9f03ef5e4d5deeaa2f5d815ba0eb9e1d68ffb8e8
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-04-25 07:47:09 -0700

    renamed javasacript folder to js, added comment for login button on index, minor pseudocode on js

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+//Once login Button added
+//use this code to hide login page, and display next available container
+//$("button").click(function(){
+//    $("p").hide();
+//});
+$(document).ready(function () {
+    $("#mainMenu").hide();
+    $("#checkIn").hide();
+    $("#pictureModal").hide();
+    $("#verified").hide();
+    $("#checkList").hide();
+    $("#map").hide();
+    $("#checkOutForm").hide();
+    $("#checkConfirm").hide();
+})
