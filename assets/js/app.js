b1b00b071fd52ee5d30e85b0e4e48ba9f12f0564
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-04-27 11:01:17 -0700

    seperated main index html into seperate html documents

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,22 @@
 //$("button").click(function(){
 //    $("p").hide();
 //});
-$(document).ready(function () {
-    $(".container").hide(1);
-    $("#logIn").show(1000);
-    // $("#mainMenu").hide();
-    // $("#checkIn").hide();
-    // $("#pictureModal").hide();
-    // $("#verified").hide();
-    // $("#checkList").hide();
-    // $("#map").hide();
-    // $("#checkOutForm").hide();
-    // $("#checkConfirm").hide();
-})
+//Initialize Communication with Back-end Services
+var platform = new H.service.Platform({
+    'app_id': '{rRSwffLhH5bN7VhtEp6F}',
+    'app_code': '{Lt6LDvpaaNEjVVpm51M88Q}'
+});
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('map'),
+    defaultLayers.normal.map,
+    {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 }
+    });
+console.log(map);
+$(".container").hide();
+$("#logIn").show();
