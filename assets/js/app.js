295a5702e16ce5681e4b065b3a414221cacf128e
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-04-27 13:56:13 -0700

    separated elements into seperate html files, added extra html file to test out maps, maps is loading no option to input search

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,11 @@
 //$("button").click(function(){
 //    $("p").hide();
 //});
-//Initialize Communication with Back-end Services
-var platform = new H.service.Platform({
-    'app_id': '{rRSwffLhH5bN7VhtEp6F}',
-    'app_code': '{Lt6LDvpaaNEjVVpm51M88Q}'
-});
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-    document.getElementById('map'),
-    defaultLayers.normal.map,
-    {
-        zoom: 10,
-        center: { lat: 52.5, lng: 13.4 }
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
     });
+}
 console.log(map);
-$(".container").hide();
-$("#logIn").show();
