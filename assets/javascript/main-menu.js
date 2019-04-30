b7098dc8dfb6d3562bd146a2a13c80c4f41667cd
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-04-29 20:55:54 -0700

    initial js files for each page

---
+++ assets/javascript/main-menu.js
@@ -0,0 +1,26 @@
+var config = {
+    apiKey: "AIzaSyCL-ojfz_0NOd_iH9B0oIcjoQUA1BsKh8Y",
+    authDomain: "lasafeparking.firebaseapp.com",
+    databaseURL: "https://lasafeparking.firebaseio.com",
+    projectId: "lasafeparking",
+    storageBucket: "lasafeparking.appspot.com",
+    messagingSenderId: "647414711077"
+    };
+    firebase.initializeApp(config);
+
+$(document).ready(function() {
+
+
+
+
+
+    
+});
+
+
+
+
+
+
+
+
