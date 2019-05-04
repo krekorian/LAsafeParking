437cbaeb1140b45ada1cf874b0d087bdbbe3216a
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-05-04 09:59:04 -0700

    modified my files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,68 @@
-//Once login Button added
-//use this code to hide login page, and display next available container
-//$("button").click(function(){
-//    $("p").hide();
-//});
-var map;
+
+//Must have added src incorrect will not load once i add into document load function
+//will keep outside until I figure it out 
+//adding maps js
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: { lat: 34.052235, lng: -118.2437 }
+    });
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+    var control = document.getElementById('floating-panel');
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+    var onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    directionsService.route({
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING'
+    }, function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
     });
 }
-console.log(map);
+//will load code on page load \
+$(function () {
+    //login js
+    $("#login").on("click", function login() {
+
+        // prevents default form 
+        event.preventDefault();
+
+        // create variables to store the userId and userPass values 
+        var userLogin = $("#userId").val() + '@gmail.com';
+        var userPassword = $("#userPass").val();
+
+        console.log(userLogin);
+        console.log(userPassword);
+
+        alert("youre logged in");
+
+        // error code to be returned if login not successful
+        firebase.auth().signInWithEmailAndPassword(userLogin, userPassword).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            alert("Error:" + errorMessage);
+        });
+
+    })
+    //add new page js here
+});
