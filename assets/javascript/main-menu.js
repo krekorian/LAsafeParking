6fd1ef2669c28ffebacc367033f733c103bf3e99
krekorian@gmail.com <krekorian@gmail.com>
2019-05-04 14:52:34 -0700

    data populating and check out button code for Rachel

--- assets/javascript/main-menu.js
+++ assets/javascript/main-menu.js
@@ -33,6 +33,41 @@ $(document).ready(function () {
 
 });
 
+//below code for checkout
+
+$("#<button id>").on("click", function () {
+    //  *************************
+    //  *************************
+    // get car name using button attribute
+    //  *************************
+    //  *************************
+
+    var ref = firebase.database().ref("lot123"); // replace "lot123" with a variable that is grabbed when log in
+
+    var check_license = $(this).attr("name");  //this attribute should match the attribute on the button, you can user
+    // calculater in class activity as a reference
+
+    //updating firebase database
+    ref.once("value")
+        .then(function (snapshot) {
+            var updates = {};
+
+            updates['/lot123/7EFZ117/checked'] = "no";
+            firebase.database().ref().update(updates);
+
+        });
+});
+
+// use below to popuplate cars that checked in
+var ref = firebase.database().ref("lot123");  // replace "lot123" with a variable that is grabbed when log in
+var ischecked
+ref.on("child_added", function (childSnapshot) {
+    var license = childSnapshot.val().license_plate_number;
+    var checked = childSnapshot.val().checked
+
+    console.log(license, checked);
+});
+
 
 
 
