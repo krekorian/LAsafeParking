f13d27d5bac36295c79091ac500119936376f4ea
krekorian@gmail.com <krekorian@gmail.com>
2019-05-03 17:29:13 -0700

    firebase local JS push

---
+++ assets/javascript/firebase-local.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCL-ojfz_0NOd_iH9B0oIcjoQUA1BsKh8Y",
+    authDomain: "lasafeparking.firebaseapp.com",
+    databaseURL: "https://lasafeparking.firebaseio.com",
+    projectId: "lasafeparking",
+    storageBucket: "lasafeparking.appspot.com",
+    messagingSenderId: "647414711077"
+};
+firebase.initializeApp(config);
+var dataRef = firebase.database();
