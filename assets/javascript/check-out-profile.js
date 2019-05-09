34807cfed273ae6685ae1b936c4690e5cddf5a88
alexisbbieda <alexisbbieda@gmail.com>
2019-05-08 19:23:26 -0700

    new check out profile js

---
+++ assets/javascript/check-out-profile.js
@@ -0,0 +1,49 @@
+var lotNumber = localStorage.getItem('lotNumber');
+var licensePlateNumber = localStorage.getItem('checkoutLicensePlate');
+$(document).ready(function () {
+    var storageRef = firebase.storage();
+
+
+    storageRef.ref('plateImages/' + lotNumber + '/' + licensePlateNumber + '.jpg').getDownloadURL().then(function (url) {
+
+
+        // Or inserted into an <img> element:
+        $('#plateImageUpload').html('<img id="imageUpload" src=' + url + ' />');
+    }).catch(function (error) {
+        // Handle any errors
+    });
+
+    // On click event...user clicks on check out button and is re-routed to check-out-confirm.html
+    return firebase.database().ref('/'+lotNumber + '/' + licensePlateNumber).once('value').then(function (snapshot) {
+        console.log(snapshot.val().license_plate_number);
+        $("#name").text(snapshot.val().first_name + " " + snapshot.val().last_name);
+        $("#plateAnswer").text(snapshot.val().license_plate_number);
+        $("#makeAnswer").html(snapshot.val().car_make);
+
+        $("#modelAnswer").text(snapshot.val().car_model);
+        $("#dateAnswer").text(snapshot.val().Date);
+        $("#timeAnswer").text(snapshot.val().Time);
+        $("#driverId").text(snapshot.val().SafeStayID);
+    });
+});
+
+$("#checkOutBtn").on("click", function () {
+
+    var date = new Date();
+        var time = new Date().toLocaleTimeString();
+        var date = new Date().toLocaleDateString();
+
+        var updates = {};
+
+        updates['/' + lotNumber + '/' + licensePlateNumber + '/' + 'checked'] = "N/A";
+        updates['/' + lotNumber + '/' + licensePlateNumber + '/' + 'Time'] = time;
+        updates['/' + lotNumber + '/' + licensePlateNumber + '/' + 'Date'] = date;
+        firebase.database().ref().update(updates);
+        window.location.href = "./check-out-confirm.html";
+});
+
+ // On click event listener for logout button to take you back to login page.
+ $("#logout").on("click", function logout() {
+    firebase.auth().signOut();
+    window.location.href = "./login.html"
+});
