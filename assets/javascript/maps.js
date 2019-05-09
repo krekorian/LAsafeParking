8e066f52ae8f38f197901a5c7e16fa6aaa0ba5d6
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-05-09 12:37:37 -0700

    changed div of map to html instead of java to help prevent map load on mobile, added styling to upload button for mobile

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,134 +1,139 @@
-$(document).ready(function () {
-    var panelDiv = $("<div class='container'>");
-    panelDiv.attr("id", "floating-panel");
-    var st = $("<strong>Start:</strong>");
-    var s = $("<select id='start'>");
-    var b = $("<br>");
-    var en = $("<strong>End:</strong>");
-    var e = $("<select id='end'>");
-    panelDiv.append(st);
-    panelDiv.append(s);
-    panelDiv.append(b);
-    panelDiv.append(en);
-    panelDiv.append(e);
-    var mapDiv = $("<div id='map'>");
-    var rightDiv = $("<div id='right-panel'>");
-    $(".container").after(rightDiv);
-    $(".container").after(mapDiv);
-    $(".container").after(panelDiv);
-    //your code here
-    //easy way "<option value="addres">TEXT</option>"
-    var startAdd = [
-        {
-            address: "",
-            text: "Select Lot"
-        },
-        {
-            address: "ucla los angeles, ca ",
-            text: "UCLA"
-        },
-        {
-            address: "100 Universal City Plaza, Universal City, CA 91608",
-            text: "Studio City"
-        },
-        {
-            address: "200 Santa Monica Pier, Santa Monica, CA 90401",
-            text: "Santa Monica"
-        },
-        {
-            address: "6925 Hollywood Blvd, Hollywood, CA 90028",
-            text: "Hollywood"
-        },
-        {
-            address: "3835 Cross Creek Rd, Malibu, CA 90265",
-            text: "Malibu"
-        },
-        {
-            address: "231 S Grevillea Ave, Inglewood, CA 90301",
-            text: "Inglewood"
-        },
-        {
-            address: "7850 Melrose Ave, Los Angeles, CA 90046",
-            text: "Fairfax"
-        }
-    ];
-
-    function createOptions() {
-        $("#start").empty();
-        //create array to hold start <option> tags
-        var newOptions = [];
-        for (var i = 0; i < startAdd.length; i++) {
-            var newOption = $('<option>');
-            newOption.attr("value", startAdd[i].address); // add [i] here to index element
-            newOption.text(startAdd[i].text); // add [i] here to index element
-            newOptions.push(newOption); // add new option to the array
-        }
-        //appending=expensive, keep array of elements and then use .html to add array to <select>
-        $("#start").html(newOptions); // once loop is complete, add the array of elements to the DOM
-
+//var mapDiv = $("<div id='map'>");
+$(document).ready(function() {
+  var panelDiv = $("<div class='container'>");
+  panelDiv.attr("id", "floating-panel");
+  var st = $("<strong>Start:</strong>");
+  var s = $("<select id='start'>");
+  var b = $("<br>");
+  var en = $("<strong>End:</strong>");
+  var e = $("<select id='end'>");
+  panelDiv.append(st);
+  panelDiv.append(s);
+  panelDiv.append(b);
+  panelDiv.append(en);
+  panelDiv.append(e);
+  //var mapDiv = $("<div id='map'>");
+  var rightDiv = $("<div id='right-panel'>");
+  $("#map").after(rightDiv);
+  //$(".container").after(mapDiv);
+  $("#map").before(panelDiv);
+  //your code here
+  //easy way "<option value="addres">TEXT</option>"
+  var startAdd = [
+    {
+      address: "",
+      text: "Select Lot"
+    },
+    {
+      address: "ucla los angeles, ca ",
+      text: "UCLA"
+    },
+    {
+      address: "100 Universal City Plaza, Universal City, CA 91608",
+      text: "Studio City"
+    },
+    {
+      address: "200 Santa Monica Pier, Santa Monica, CA 90401",
+      text: "Santa Monica"
+    },
+    {
+      address: "6925 Hollywood Blvd, Hollywood, CA 90028",
+      text: "Hollywood"
+    },
+    {
+      address: "3835 Cross Creek Rd, Malibu, CA 90265",
+      text: "Malibu"
+    },
+    {
+      address: "231 S Grevillea Ave, Inglewood, CA 90301",
+      text: "Inglewood"
+    },
+    {
+      address: "7850 Melrose Ave, Los Angeles, CA 90046",
+      text: "Fairfax"
     }
-    createOptions();
+  ];
 
-    //for end selection
-    function createEndOptions() {
-        $("#end").empty();
-        //create array to hold start <option> tags
-        var newOptions = [];
-        for (var i = 0; i < startAdd.length; i++) {
-            var newOption = $('<option>');
-            newOption.attr("value", startAdd[i].address); // add [i] here to index element
-            newOption.text(startAdd[i].text); // add [i] here to index element
-            newOptions.push(newOption); // add new option to the array
-        }
-        //appending=expensive, keep array of elements and then use .html to add array to <select>
-        $("#end").html(newOptions); // once loop is complete, add the array of elements to the DOM
-
+  function createOptions() {
+    $("#start").empty();
+    //create array to hold start <option> tags
+    var newOptions = [];
+    for (var i = 0; i < startAdd.length; i++) {
+      var newOption = $("<option>");
+      newOption.attr("value", startAdd[i].address); // add [i] here to index element
+      newOption.text(startAdd[i].text); // add [i] here to index element
+      newOptions.push(newOption); // add new option to the array
     }
-    createEndOptions();
+    //appending=expensive, keep array of elements and then use .html to add array to <select>
+    $("#start").html(newOptions); // once loop is complete, add the array of elements to the DOM
+  }
+  createOptions();
 
+  //for end selection
+  function createEndOptions() {
+    $("#end").empty();
+    //create array to hold start <option> tags
+    var newOptions = [];
+    for (var i = 0; i < startAdd.length; i++) {
+      var newOption = $("<option>");
+      newOption.attr("value", startAdd[i].address); // add [i] here to index element
+      newOption.text(startAdd[i].text); // add [i] here to index element
+      newOptions.push(newOption); // add new option to the array
+    }
+    //appending=expensive, keep array of elements and then use .html to add array to <select>
+    $("#end").html(newOptions); // once loop is complete, add the array of elements to the DOM
+  }
+  createEndOptions();
 });
 
 function initMap() {
-    //used for directions output
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    //will display map image on <div id="map">
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        //LA
-        center: { lat: 34.052235, lng: -118.2437 },
-        //remove buttons on map
-        disableDefaultUI: true
-    });
-    directionsDisplay.setMap(map);
-    //direction set on this element
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
+  //used for directions output
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+  //will display map image on <div id="map">
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    //LA
+    center: { lat: 34.052235, lng: -118.2437 },
+    //remove buttons on map
+    disableDefaultUI: true
+  });
+  directionsDisplay.setMap(map);
+  //direction set on this element
+  directionsDisplay.setPanel(document.getElementById("right-panel"));
 
-    var onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    // [Violation] Added non-passive event listener to a scroll-blocking 'touchstart' event. Consider marking event handler as 'passive' to make the page more responsive.
-    //fixed violation by making onChangeHandler { passive: true }
-    document.getElementById('start').addEventListener('change', onChangeHandler, { passive: true });
-    document.getElementById('end').addEventListener('change', onChangeHandler, { passive: true });
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  // [Violation] Added non-passive event listener to a scroll-blocking 'touchstart' event. Consider marking event handler as 'passive' to make the page more responsive.
+  //fixed violation by making onChangeHandler { passive: true }
+  document
+    .getElementById("start")
+    .addEventListener("change", onChangeHandler, { passive: true });
+  document
+    .getElementById("end")
+    .addEventListener("change", onChangeHandler, { passive: true });
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        }
-    });
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  directionsService.route(
+    {
+      origin: start,
+      destination: end,
+      travelMode: "DRIVING"
+    },
+    function(response, status) {
+      if (status === "OK") {
+        directionsDisplay.setDirections(response);
+      }
+    }
+  );
 }
 
 $("#logout").on("click", function logout() {
-    firebase.auth().signOut();
-    localStorage.clear();
-    window.location.href = "./login.html"
+  firebase.auth().signOut();
+  localStorage.clear();
+  window.location.href = "./login.html";
 });
